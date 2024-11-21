9c27b35de5a7c8ce2f66eafd6274886bfbaf20c8
Baptiste Duval <bduval@bess-f2r1s13.clusters.42paris.fr>
2024-11-21 17:46:31 +0100

    Thu Nov 21 05:46:31 PM CET 2024

--- getNextLine/get_next_line.c
+++ getNextLine/get_next_line.c
@@ -6,98 +6,50 @@
 /*   By: bduval <bduval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 17:15:25 by bduval            #+#    #+#             */
-/*   Updated: 2024/11/21 14:14:45 by bduval           ###   ########.fr       */
+/*   Updated: 2024/11/21 17:40:46 by bduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(char *s)
-{
-	size_t	len;
-
-	if (!s)
-		return (0);
-	len = 0;
-	while (s[len])
-	{
-		len++;
-		if (len > 0 && s[len - 1] == '\n')
-			return (len);
-	}
-	return (len);
-}
-
-char	*ft_init_new(char *res, char *buff)
-{
-	size_t	lr;
-	size_t	lb;
-	char	*new;
-
-	lr = ft_strlen(res);
-	lb = ft_strlen(buff);
-	new = (char *) malloc(lr + lb + 1);
-	if (!new)
-		return (NULL);
-	return (new);
-}
-
-void	ft_addchr(char *to, char *from)
-{
-	*to = *from;
-	*++to = 0;
-	*from = 0;
-	return ;
-}
-
-char	*ft_addto(char *res, char *buff, char mem[BUFFER_SIZE], char *isendl)
+char	*ft_addto(char *res, char *buff, char mem[BUFFER_SIZE + 1], char *isendl)
 {
 	char	*new;
 	int		i;
 	int		j;
+	size_t	lr;
 
-	i = 0;
 	new = ft_init_new(res, buff);
-	while (res && res[i])
-	{
-		new[i] = res[i];
-		i++;
-	}
+	lr = ft_strlen(res);
+	ft_move_to_new(res, new, &i);
 	j = 0;
 	*isendl = 0;
-	while (buff[i - ft_strlen(res)])
-	{
+	while (buff[i - lr])
 		if (!*isendl)
 		{
-			if (buff[i - ft_strlen(res)] == '\n' && !*isendl)
+			if (buff[i - lr] == '\n' && !*isendl)
 				*isendl = 1;
-			new[i] = buff[i - ft_strlen(res)];
-			buff[i - ft_strlen(res)] = 0;
-			new[i + 1] = '\0';
+			ft_addchr(&new[i], &buff[i - lr]);
 			i++;
 		}
 		else
 		{
-			mem[j] = buff[i - ft_strlen(res)];
-			buff[i - ft_strlen(res)] = 0;
+			ft_addchr(&mem[j++], &buff[i++ - lr]);
 			*isendl = -1;
-			j++;
-			i++;
 		}
-	}
 	free(res);
 	return (new);
 }
 
 char	*get_next_line(int fd)
 {
-	char		buff[BUFFER_SIZE];
-	static char	mem[BUFFER_SIZE];
+	char		buff[BUFFER_SIZE + 1];
+	static char	mem[BUFFER_SIZE + 1];
 	int			rd;
 	char		stop;
 	char		*res;
 
-	buff[BUFFER_SIZE - 1] = '\0';
+	ft_bzero(buff, BUFFER_SIZE + 1);
 	res = NULL;
 	if (*mem)
 	{
@@ -107,12 +59,12 @@ char	*get_next_line(int fd)
 	}
 	else 
 		stop = 0;
-	rd = BUFFER_SIZE - 1;
-	while (rd == BUFFER_SIZE - 1 && !stop)
+	rd = BUFFER_SIZE;
+	while (rd == BUFFER_SIZE && !stop)
 	{
-		rd = read(fd, buff, BUFFER_SIZE - 1);
+		rd = read(fd, buff, BUFFER_SIZE);
 		if (rd <= 0 && !stop)
-			return (NULL);
+			return (res);
 		res = ft_addto(res, buff, mem, &stop);
 	}
 	return (res);
--- getNextLine/get_next_line_utils.c
+++ getNextLine/get_next_line_utils.c
@@ -6,20 +6,68 @@
 /*   By: bduval <bduval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 14:10:31 by bduval            #+#    #+#             */
-/*   Updated: 2024/11/21 10:41:26 by bduval           ###   ########.fr       */
+/*   Updated: 2024/11/21 17:36:33 by bduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	ft_bzero(void *mem, size_t bytes)
+void	ft_bzero(char *mem, size_t bytes)
 {
-	unsigned int	i;
-	unsigned char	*p_mem;
-
-	i = 0;
-	p_mem = (unsigned char *)mem;
-	while (i < bytes)
-		p_mem[i++] = 0;
-	write(1, "LA \n", 4);
+	while (bytes--)
+		*mem = 0;
 }
+
+size_t	ft_strlen(char *s)
+{
+	size_t	len;
+
+	if (!s)
+		return (0);
+	len = 0;
+	while (s[len])
+	{
+		len++;
+		if (len > 0 && s[len - 1] == '\n')
+			return (len);
+	}
+	return (len);
+}
+
+char	*ft_init_new(char *res, char *buff)
+{
+	size_t	lr;
+	size_t	lb;
+	char	*new;
+
+	lr = ft_strlen(res);
+	lb = ft_strlen(buff);
+	new = (char *) malloc(lr + lb + 1);
+	if (!new)
+		return (NULL);
+	new[lr + lb] = '\0';
+	return (new);
+}
+
+void	ft_addchr(char *to, char *from)
+{
+	if (!to)
+		return ;
+	*to = *from;
+	*(++to) = 0;
+	*from = 0;
+	return ;
+}
+
+void	ft_move_to_new(char *res, char *new, int *i)
+{
+	if (!new)
+		return ;
+	*i = 0;
+	while (res && res[*i])
+	{
+		ft_addchr(&new[*i], &res[*i]);
+		(*i)++;
+	}
+}
+
