99fc86b9616d6d07de5e0d07e01198f8ae0dc66b
1Batmain <batduval@gmail.com>
2025-05-13 17:44:14 +0200

    fin

--- philosopher/philo/src/eat.c
+++ philosopher/philo/src/eat.c
@@ -6,7 +6,7 @@
 /*   By: bduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 22:21:40 by bduval            #+#    #+#             */
-/*   Updated: 2025/05/07 12:20:48 by bduval           ###   ########.fr       */
+/*   Updated: 2025/05/13 17:42:13 by bduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ static int	take_forks(t_table *table, t_philo *philo)
 {
 	if (philo->right != philo->left)
 	{
-		pthread_mutex_lock(&table->fork_lock[philo->left]);
-		print_fork(table, philo);
 		pthread_mutex_lock(&table->fork_lock[philo->right]);
+		print_fork(table, philo);
+		pthread_mutex_lock(&table->fork_lock[philo->left]);
 		print_fork(table, philo);
 		return (1);
 	}
@@ -35,14 +35,14 @@ static int	take_forks(t_table *table, t_philo *philo)
 
 static void	put_forks_back(t_table *table, t_philo *philo)
 {
-	pthread_mutex_unlock(&table->fork_lock[philo->right]);
 	pthread_mutex_unlock(&table->fork_lock[philo->left]);
+	pthread_mutex_unlock(&table->fork_lock[philo->right]);
 }
 
 static void	eat(t_table *table, t_philo *philo)
 {
 	if (!game_is_on(table, philo))
-		return (put_forks_back(table, philo));
+		return ;
 	print_eat(table, philo);
 	pthread_mutex_lock(&philo->lock);
 	gettimeofday(&philo->last_meal, NULL);
